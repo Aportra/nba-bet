06075becba41e68d77a1415f75056daba50b5f36
Aaron Portra <aportra99@gmail.com>
2024-12-22 11:04:55 -0800

    still debuggin automatic scraper, getting around blocks

--- main.py
+++ main.py
@@ -36,15 +36,11 @@ def establish_driver():
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # Prevent detection
     chrome_options.add_argument("--headless")  # Enable headless mode for CI/CD
     chrome_options.add_argument("--disable-popup-blocking")
-    chrome_options.add_argument("--disable-javascript")  # Optional: disable JavaScript
+    # chrome_options.add_argument("--disable-javascript")  # Optional: disable JavaScript
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
     )
 
-
-    chrome_options.add_argument(
-        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
-    )
     
    
     driver = uc.Chrome(options = chrome_options)
