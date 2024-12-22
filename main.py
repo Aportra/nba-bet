1dd6760afbaf19cb9d99ad6b80f375ae266d5474
Aaron Portra <aportra99@gmail.com>
2024-12-22 14:24:57 -0800

    working local

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from subprocess import getoutput
 from bs4 import BeautifulSoup
