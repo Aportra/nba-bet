f42b074329111e74219f005288fc654d12117a12
Aaron Portra <aportra99@gmail.com>
2024-12-22 19:58:22 -0800

    might use api

--- main.py
+++ main.py
@@ -1,157 +1,25 @@
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
-from subprocess import getoutput
-from bs4 import BeautifulSoup
-import regex as re
 import time
 from datetime import datetime as date
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from nba_api.stats.endpoints import LeagueGameFinder, BoxScoreTraditionalV2
+from nba_api.stats.library.parameters import SeasonAll
 import os
 
 
+def get_past_games(season):
+    game_finder = LeagueGameFinder(season_nullable=season,season_type_nullable='Regular Season')
+    game = game_finder.get_data_frames()[0]
+    return game[['GAME_ID', 'GAME_DATE', 'MATCHUP']]
 
-def establish_driver():
-    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-
-    chrome_options = Options()
-    options = [
-        "--headless",
-        "--disable-gpu",
-        "--window-size=1920,1200",
-        "--ignore-certificate-errors",
-        "--disable-extensions",
-        "--no-sandbox",
-        "--disable-dev-shm-usage"
-    ]
-    for option in options:
-    chrome_options.add_argument(option)
-
-    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-    driver = webdriver.Chrome(service=service, options=options)
-
-    return driver
-
-#Select all option only works when at least half screen due to blockage of the all option when not in headerless option
-
-def select_all_option(driver):
+def get_box_score(game_id):
     try:
-        # Click the dropdown
-
-        dropdown = WebDriverWait(driver,10).until(
-            EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[2]/div[1]/div[3]/div/label"))
-        )
-        driver.execute_script("arguments[0].click();", dropdown)
-        # Click the "All" option
-        all_option = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[2]/div[1]/div[3]/div/label/div/select/option[1]"))
-        )
-        all_option.click()
-
-        print("Successfully selected the 'All' option.")
-    except Exception as e:
-        print(f"Error selecting the 'All' option: {e}")
-
-
-def process_page(page,game_id,game_date,home,away,driver):
-    driver.get(page)
-    
-    driver.set_page_load_timeout(120)
-    driver.implicitly_wait(10)
-
-    ps = driver.page_source
-    soup = BeautifulSoup(ps, 'html5lib')
-    
-    # Find all divs containing the data tables
-    tables = soup.find_all('table', id=lambda x: x and "game-basic" in x, class_='sortable stats_table now_sortable')
-    last_updated = date.today()
-    df_data = []
-    # Check if tables exist
-    if tables:
-        for table_index, table in enumerate(tables):
-            # Extract the table rows
-            rows = table.find_all('tr')
-            if table_index == 1:
-                t1 = home
-                t2 = away
-            else:
-                t1 = away
-                t2 = home
-            # Get the header row (if it exists)
-            thead = table.find('thead')
-            if thead:
-                header_row = thead.find_all('tr')[1]  # Second <tr> in <thead>
-                headers = [th.get_text(strip=True) for th in header_row.find_all('th')]
-                headers.extend(['team','game_id','game_date','matchup','url','last_updated'])
-            else:
-                headers = []  # Handle case if no headers are present
-
-            tfoot = table.find('tfoot')
-            tfoot_rows = []
-            if tfoot:
-                tfoot_rows = tfoot.find_all('tr')
-            # Get the data rows
-            data = []
-            
-            for row in rows:  # Skip the header row
-                if row in tfoot_rows:
-                    print(f"Skipping row in tfoot: {row.get_text(strip=True)}")
-                    continue
-                if 'thead' in row.get('class', []):
-                    continue
-
-                reason_cell = row.find('td', {'colspan': True})
-                if reason_cell:
-                    # Log the skipped row for debugging
-                    print(f"Skipping row due to colspan: {reason_cell.get_text(strip=True)}")
-                    continue
-                
-                first_cell = row.find('th')
-                first_value = first_cell.get_text(strip=True) if first_cell else None
-                if first_value in ["Starters", "Reserves", "Team Totals", ""] or not first_value:
-                    print(f"Skipping label or invalid row: {first_value}")
-                    continue
-                # Extract all <td> elements from the row
-                cols = row.find_all('td')
-                row_data = [col.get_text(strip=True) for col in cols]
-                row_data.extend([t1, game_id, game_date, t2, page, last_updated])
-                # Prepend the first <th> value to the row
-                if first_value:
-                    row_data.insert(0, first_value)
-
-                # Check row length
-                if len(row_data) < len(headers):  # Pad short rows
-                    print(f"Padding row: {row_data}")
-                    row_data.extend([None] * (len(headers) - len(row_data)))
-                elif len(row_data) > len(headers):  # Trim long rows
-                    print(f"Trimming row: {row_data}")
-                    row_data = row_data[:len(headers)]
-                data.append(row_data)
-    
-            
-            # Create a DataFrame for this table
-            if headers and data:
-                
-                df = pd.DataFrame(data, columns=headers)
-            else:
-                df = pd.DataFrame(data)  
-            
-            df_data.append(df)
-        
-        final_df = pd.concat(df_data,ignore_index=True)
-            # Append the DataFrame to the appropriate team entries in the dictionary
-        return final_df
-    else:
-        print(f'Could not process: {page}')
-        return game_id,game_date,home,away
+        box_score = BoxScoreTraditionalV2(game_id=game_id)
+    except:
+        return game_id
+    return box_score.player_stats.get_data_frame()
 
 def send_email(subject,body):
     sender_email = os.getenv('SERVER_EMAIL')
@@ -178,6 +46,3 @@ def send_email(subject,body):
         server.quit()
 
 #Makes it so we are not connecting to driver on import
-if __name__ == "__main__":
-    driver = establish_driver()
-    select_all_option(driver)
--- scrape_past_games.py
+++ scrape_past_games.py
@@ -1,131 +1,39 @@
 import pandas as pd
 import main
 from datetime import datetime as dt
-from selenium.webdriver.common.by import By
-from selenium import webdriver
+from nba_api.stats.endpoints import LeagueGameFinder, BoxScoreTraditionalV2
+from nba_api.stats.library.parameters import SeasonAll
+import pandas as pd
 import time
 import pandas_gbq
 from google.cloud import bigquery
 
 # Basketball Reference URLs
-urls = {
-    'NBA_Season_2021-2022_': 'https://www.basketball-reference.com/leagues/NBA_2022_games-',
-    'NBA_Season_2022-2023_': 'https://www.basketball-reference.com/leagues/NBA_2023_games-',
-    'NBA_Season_2023-2024_': 'https://www.basketball-reference.com/leagues/NBA_2024_games-',
-    'NBA_Season_2024-2025_': 'https://www.basketball-reference.com/leagues/NBA_2025_games-',
-}
+seasons = ['2021-22','2022-23','2023-24','2024-25']
 
-months = ['October', 'November', 'December', 'January', 'February', 'March', 'April']
+data = []
 
-valid_time_pattern = r"^\d{1,2}:\d{1,2}$"
+for season in seasons:
+    game_id = []
+    failed_games = []
+    game_id.append(main.get_past_games(season))
 
-driver = webdriver.Firefox()
-
-for url_key, base_url in urls.items():
-    game_data = []
-    unique_game_ids = set()
-
-    for month in months[:1]:
-        driver.get(f"{base_url}{month.lower()}.html")
-        time.sleep(2)  # Allow page to load
-
-        rows = driver.find_elements(By.XPATH, "//table[@id='schedule']/tbody/tr")
-
-        for row in rows[:1]:
-            # Skip header rows
-            if "thead" in row.get_attribute("class"):
-                continue
-
-            # Extract game date
-            date_element = row.find_element(By.XPATH, "./th[@data-stat='date_game']")
-            game_date_text = date_element.text.strip()
-            print(game_date_text)
-
-            try:
-                # Parse the date using the existing format
-                game_date = dt.strptime(game_date_text, "%a, %b %d, %Y").date()
-            except ValueError:
-                print(f"Invalid date format: {game_date_text}")
-                continue  # Skip invalid dates
-
-            # Stop if the game is in the future
-            if game_date > dt.today().date():
-                break
-
-            # Extract matchup data
-            try:
-                visitor_team = row.find_element(By.XPATH, "./td[@data-stat='visitor_team_name']").text.strip()
-                home_team = row.find_element(By.XPATH, "./td[@data-stat='home_team_name']").text.strip()
-                game_id = row.find_element(By.XPATH, "./td[@data-stat='box_score_text']/a").get_attribute("href")
-            except Exception:
-                continue  # Skip rows with missing data
-
-            if game_id in unique_game_ids:
-                continue
-
-            unique_game_ids.add(game_id)
-            game_data.append((game_id, game_date, home_team, visitor_team))
-
-    # Process game data
-    data = []
-    failed_pages = []
-    retries = {}
-
-    for i, (game_id, game_date, home, away) in enumerate(game_data):
-        print(game_id)
-        page = f"{game_id}"
-        if len(data) % 100:
-            print(f"Processing {i} requests: {round(len(data)/len(game_data)*100, 2)}% complete")
-
-        result = main.process_page(page, game_id, game_date, home, away, driver)
-        if isinstance(result, pd.DataFrame):
-            data.append(result)
+    for game in game_id:
+        time.sleep(1)
+        g = main.get_box_score(game)
+        if isinstance(g,pd.DataFrame):
+            data.append(main.get_box_score(game))
         else:
-            failed_pages.append((game_id, game_date, home, away))
-            print(f"Failed Pages length: {len(failed_pages)}")
-
-    # Retry failed pages
-    while failed_pages:
-        game_id, game_date, home, away = failed_pages.pop(0)
-        key = (game_id, game_date, home, away)
-
-        retries[key] = retries.get(key, 0) + 1
-        print(f"Retry Count: {retries[key]}")
-
-        page = f"{game_id}"
-        result = main.process_page(page, game_id, game_date, home, away, driver)
-
-        if isinstance(result, pd.DataFrame):
-            data.append(result)
-            print(f"Processed {game_id} from failed pages")
+            failed_games.append(game)
+    while failed_games:
+        game = failed_games.pop(0)
+        g = main.get_box_score(game)
+        if isinstance(g,pd.DataFrame):
+            data.append(main.get_box_score(game))
         else:
-            failed_pages.append((game_id, game_date, home, away))
-            print(f"Failed {game_id} again, re-added to retry list")
+            failed_games.append(game)
 
-    # Combine dataframes for upload
-    combined_dataframes = pd.concat(data, ignore_index=True)
+    combined_df = pd.concat(data,ignore_index= True)
 
-    # BigQuery client setup
-    client = bigquery.Client(project="miscellaneous-projects-444203")
+    pandas_gbq.to_gbq(combined_df,project_id='miscellaneous-projects-444203',destination_table=f'basketball_reference.{season}',if_exists='replace')
 
-    combined_dataframes.rename(columns={'+/-': 'plus_minus'}, inplace=True)
-
-    # Correct invalid rows
-    # invalid_rows = ~combined_dataframes['MIN'].str.match(valid_time_pattern, na=False)
-    # columns_to_swap = ['FGM', 'FGA', 'FG%', '3PM', '3PA', '3P%']
-    # valid_columns = ['team', 'game_id', 'game_date', 'matchup', 'url', 'last_updated']
-
-    # combined_dataframes.loc[invalid_rows, valid_columns] = combined_dataframes.loc[invalid_rows, columns_to_swap].values
-    # combined_dataframes.loc[invalid_rows, columns_to_swap] = None
-
-    # # Fix data types
-    # combined_dataframes['game_date'] = pd.to_datetime(combined_dataframes['game_date'], errors='coerce')
-    # combined_dataframes['last_updated'] = pd.to_datetime(combined_dataframes['last_updated'], errors='coerce')
-    # combined_dataframes['url'] = combined_dataframes['url'].astype(str).str.strip()
-    # combined_dataframes['game_id'] = combined_dataframes['game_id'].str.replace('https://www.basketball-reference.com/', '')
-
-    # num_columns = ['FGM', 'FGA', 'FG%', '3PM', '3PA', '3P%', 'FTM', 'FTA', 'FT%', 'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK', 'TO', 'PF', 'PTS', 'plus_mins']
-    # combined_dataframes[num_columns] = combined_dataframes[num_columns].apply(pd.to_numeric, errors='coerce')
-
-    # Upload to BigQuery
-    pandas_gbq.to_gbq(combined_dataframes, project_id="miscellaneous-projects-444203", destination_table=f'basketball_reference.{url_key}_basketball_ref', if_exists='replace')
