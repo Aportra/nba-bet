070ab063df909f921e2194e8e8065127343ce9aa
Aaron Portra <aportra99@gmail.com>
2025-01-07 21:59:55 -0800

    configuring main

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from scraping_data.scrape_games import scrape_games, scrape_team_data
+
+print("Starting scraping of game data")
+
+scrape_games()
+
+print("Starting scraping of team data")
+
+scrape_team_data()
