7cbc4cd3c2dd6ba812f27a398f6be274a41d643e
Aportra <ap222179@umconnect.umt.edu>
2025-04-19 17:37:07 -0700

    updated code for new api nba deployed

--- models/predict_new_games.py
+++ models/predict_new_games.py
@@ -52,7 +52,7 @@ def fetch_bigquery_data(query,credentials):
 def gather_data_to_model():
     """Fetches today's NBA schedule from BigQuery."""
     query = """
-    SELECT team, opponent, game_date
+    SELECT team, opponent, date(GAME_DATE_EST) as game_date
     FROM `capstone_data.schedule`
     WHERE date(GAME_DATE_EST) = CURRENT_DATE('America/Los_Angeles')
     """
